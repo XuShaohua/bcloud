ec2dd91b3db8f6ddb0d3915e38b2d3c89575b522
LiuLang <gsushzhsosgsu@gmail.com>
2014-03-22 23:04:58 +0800

    TypeError: __init__() missing 1 required positional argument: "label"

--- bcloud/Widgets.py
+++ bcloud/Widgets.py
@@ -9,14 +9,12 @@ class LeftLabel(Gtk.Label):
     '''左对齐的标签'''
 
     def __init__(self, label):
-        super().__init__()
-        self.set_text(label)
+        super().__init__(label)
         self.props.xalign = 0.0
 
 class SelectableLeftLabel(LeftLabel):
     '''左对齐的标签, 标签内容可选中'''
 
     def __init__(self, label):
-        super().__init__()
-        self.set_text(label)
+        super().__init__(label)
         self.props.selectable = True
