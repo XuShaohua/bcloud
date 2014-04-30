5ffc6352e0d125a57d4d349a6c8e30a37f2d7a7e
LiuLang <gsushzhsosgsu@gmail.com>
2014-05-01 07:19:02 +0800

    v3.3.4 released

--- bcloud/Config.py
+++ bcloud/Config.py
@@ -27,7 +27,7 @@ _ = gettext.gettext
 
 DBUS_APP_NAME = 'org.liulang.bcloud'
 APPNAME = 'BCloud'
-VERSION = '3.3.3'
+VERSION = '3.3.4'
 HOMEPAGE = 'https://github.com/LiuLang/bcloud'
 AUTHORS = ['LiuLang <gsushzhsosgsu@gmail.com>', ]
 COPYRIGHT = 'Copyright (c) 2014 LiuLang'
