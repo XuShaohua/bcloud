4dd5749625a6d798663e4d9c4099275aef17c8c8
LiuLang <gsushzhsosgsu@gmail.com>
2014-05-27 19:31:54 +0800

    Add binascii

--- bcloud/decoder.py
+++ bcloud/decoder.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed by GPLv3 license that can be found
 # in http://www.gnu.org/licenses/gpl-3.0.html
 
+import binascii
 import base64
 
 def decode_flashget(link):
