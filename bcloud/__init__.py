91602287c7e1b8c0a3cfcca5045ff59b66fa8f34
Xu Shaohua <xushaohua@linuxdeepin.com>
2017-01-22 17:11:48 +0800

    Remove python version

--- bcloud/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# Copyright (C) 2014-2015 LiuLang <gsushzhsosgsu@gmail.com>
-# Use of this source code is governed by GPLv3 license that can be found
-# in http://www.gnu.org/licenses/gpl-3.0.html
-
-__version__ = '3.8.2'
