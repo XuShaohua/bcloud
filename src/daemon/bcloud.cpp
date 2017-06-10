d3d3d420165012adc694f935e50b13b8ec908760
LiuLang <gsushzhsosgsu@gmail.com>
2017-06-10 19:12:05 +0800

    Removed python src

---
+++ src/daemon/bcloud.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2017 LiuLang. All rights reserved.
+// Use of this source is governed by General Public License that can be found
+// in the LICENSE file.
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello, world\n");
+
+  return 0;
+}
